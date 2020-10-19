67370433827630368aa97083e4fea1f0240e11f3
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-10-18 20:36:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -163,8 +163,7 @@ console.log(quickSort(arr1))
 
 const arr2 = [3, 9, 1, 14, 17, 24, 22, 20]
 console.log(quickSort(arr2))
-//last item [ 3, 9, 10, 12, 19, 14, 17, 16, 13, 15 ]
-//1st item [ 3, 9, 10, 12, 13, 14, 15, 16, 19, 17 ]
+
 
 let arr = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
 
@@ -181,17 +180,6 @@ function partition(arr, start, end) {
   }
   swap(arr, end-1, j);
   return j;
-  // let middleIndex = Math.floor((start+end)/2);
-  // let pivot = arr[middleIndex];
-  // let j = end-1;
-  // for (let i=start; i < end -1; i++) {
-  //   if (arr[i] >= pivot && arr[j] <= pivot)  {
-  //     swap(arr, i, j);
-  //     j++;
-  //   }
-  // }
-  // swap(arr, end);
-  // return j;
 }
 
 function swap(arr, i, j) {
